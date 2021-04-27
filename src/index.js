1dcb5753c180c35b743ef6b11cbbfaa722a8eea9
amir00700 <43377462+amir00700@users.noreply.github.com>
2021-04-27 13:18:36 -0400

    2.added redux and thunk to fetch data

--- src/index.js
+++ src/index.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-
 import reportWebVitals from './reportWebVitals';
+//redux setup
+import {createStore,applyMiddleware,compose} from 'redux';
+import rootReducer from './reducers';
+import {Provider} from 'react-redux';
+import thunk from 'redux-thunk';
+
+const composeEnhancer=window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store=createStore
+  (rootReducer,
+    composeEnhancer(applyMiddleware(thunk))
+ 
+  );
 
 ReactDOM.render(
   <React.StrictMode>
+    <Provider store={store}>
     <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
@@ -15,3 +29,4 @@ ReactDOM.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
